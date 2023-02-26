2663b5b080ce70edc059e62a4dedf3bdf731d14d
Rycahaya Sri Hutomo <94436711+rycahayash@users.noreply.github.com>
2023-02-26 20:54:30 +0700

    Update client_select.py

--- client/client_select.py
+++ client/client_select.py
@@ -17,7 +17,7 @@ def file_exist(header):
 
 
 def main():
-    server_address = ('192.168.1.21', 9009)
+    server_address = ('192.168.1.21', 9999)
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(server_address)
 
